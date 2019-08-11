6ea13fec2c19162d732bbc88db2ad4d067e4c77a
Shahwar Spall <shevy.spall@gmail.com>
2019-08-11 04:23:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,39 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song=self.new 
+    song.save 
+    song 
+    
+  end 
 
+def self.new_by_name(name = nil, artist = nil)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+def self.create_by_name(name)
+  song = self.create
+  song.name = name
+  song 
+end 
+
+def self.find_by_name(name)
+   self.all.find{ |song| song.name == name}
+end
+
+ def self.find_or_create_by_name(name)
+    if song = self.find_by_name(name)
+      song
+    else
+      self.create_by_name(name)
+    end
+  end
+  
+  def self.alphabetical(name)
+      self.all.sort{ |a, b| a.name <=> b.name }
+  end
 end
