260bbd1de170f78f0977a6897e7dfba303754f0d
Shahwar Spall <shevy.spall@gmail.com>
2019-08-11 04:28:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,4 +48,32 @@ end
   def self.find_by_name(name)
     self.all.find{ |song| song.name == name}
   end
+  
+  
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.new_by_name(name = nil, artist = nil)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def save
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
+  end
+end
 end
